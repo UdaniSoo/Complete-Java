24e0c7d07d6ec872f298337bf637350ca37e77e2
Udani <isuru.udani1@gmail.com>
2019-08-25 20:17:48 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Complete-Java
 Complete Java for Automation Testers from Scratch
+
+## This is second title.
+hfh
+### This is third title.
+
+This is once sentence.  
+This is sentence after enter.
+
+* ONe bullet.
+* Two bullet.
+ * Bullet in bullet.
---
+++ src/coreJavaTraiing/firstclass.java
@@ -0,0 +1,20 @@
+package coreJavaTraiing;
+
+public class firstclass {
+
+	
+	public void getData()
+	{
+		System.out.println("I am in a method");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=55 ;
+		System.out.println(a);
+		System.out.println("Im in a main method");
+
+	}
+
+}
