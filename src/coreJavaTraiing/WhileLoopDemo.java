fbf124a16e27f3d5f8ec2d455eb8ebeb68408b34
Udani <isuru.udani1@gmail.com>
2020-01-04 13:01:18 +0800

    While loop example

---
+++ src/coreJavaTraiing/WhileLoopDemo.java
@@ -0,0 +1,32 @@
+package coreJavaTraiing;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=0;
+		
+		while(i<10)
+		{
+			
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("First while loop ened");
+		
+		int j=10;
+		
+		while(j>0)
+		{
+			
+			System.out.println(j);
+			j--;
+		}
+		System.out.println("Second while loop ended");
+
+
+	}
+
+}
---
+++ src/coreJavaTraiing/forloopExample.java
@@ -0,0 +1,18 @@
+package coreJavaTraiing;
+
+public class forloopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Syntax
+		//for(initialization; condition; increment)
+		for(int i=0; i<5; i++)
+		{
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
