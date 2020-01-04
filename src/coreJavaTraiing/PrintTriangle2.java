c00be2ec56482587d650b689e0b1a92bda5a36d8
Udani <isuru.udani1@gmail.com>
2020-01-04 16:57:12 +0800

    Prining triangles

--- src/coreJavaTraiing/PrintTriangle2.java
+++ src/coreJavaTraiing/PrintTriangle2.java
@@ -5,7 +5,13 @@ public class PrintTriangle2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=0; i<=4; i++)
+		/*
+		 * 1
+		 * 1 2
+		 * 1 2 3
+		 * 1 2 3 4
+		 */
+		for(int i=1; i<=4; i++)
 		{
 			
 			for(int j=1; j<=i; j++)
@@ -16,7 +22,20 @@ public class PrintTriangle2 {
 			}
 			System.out.println("");
 		}
-
+		
+		int k=1;
+		for(int i=1; i<=4; i++)
+		{
+			
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(k);
+				System.out.print("\t");
+				k++;
+			}
+			
+			System.out.println("");
+		}
 	}
 
 }
