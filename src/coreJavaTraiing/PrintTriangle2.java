3cf1556c38db7b39bb1953f36fd4dd8cab47f5c2
Udani <isuru.udani1@gmail.com>
2020-01-04 16:31:38 +0800

    Print triangle 2

---
+++ src/coreJavaTraiing/PrintTriangle2.java
@@ -0,0 +1,22 @@
+package coreJavaTraiing;
+
+public class PrintTriangle2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i<=4; i++)
+		{
+			
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j);
+				System.out.print("\t");
+				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
